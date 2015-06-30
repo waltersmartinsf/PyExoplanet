e57ad041a907f5b19dd4cb387630b53a72f3ce45
Walter Silva Martins Filho <waltersmartinsf@gmail.com>
2015-06-30 13:56:49 -0300

    estrutura v2

--- PyExoplanet/__init__.py
+++ PyExoplanet/__init__.py
@@ -1,6 +1,26 @@
-#PyExoplanet
-#Package for data reduction and analysis for of a exoplanet
+"""
+PyExoplanet
 
+Author: Walter S. Martins-Filho
+email: walter@on.br
+___
 
+Package for data reduction and analysis for of a exoplanet
 
+___
+This package needs:
 
+- IRAF instaled in your machine
+- Pyraf
+- Numpy
+___
+
+"""
+
+#******************************************************************************
+#Data Reduction Fucntions
+#
+#This part of the package based in pyraf language for data reduction with IRAF.
+#IRAF (more in: http://iraf.noao.edu/) is a software to reduce astronomical data.
+#The Pyraf is a python interface to work with iraf inside a python script. Because
+#of that, IRAF need to be instaled before the use of this package.
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # PyExoplanet
+
+
+Author: Walter S. Martins-Filho
+email: walter@on.br
+___
+
+Package for data reduction and analysis for of a exoplanet
+
+___
+This package needs:
+
+- IRAF instaled in your machine
+- Pyraf
+- Numpy
+
 ___
 
 <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
